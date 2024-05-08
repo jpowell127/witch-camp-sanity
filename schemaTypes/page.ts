b0420e9934a9c6791120922529b71511c1a9b580
Jonathan Powell <2856365+jpowell127@users.noreply.github.com>
2024-05-08 12:42:21 -0500

    add sanity studio

---
+++ schemaTypes/page.ts
@@ -0,0 +1,24 @@
+import {defineField, defineType} from 'sanity'
+
+export default defineType({
+  name: 'page',
+  title: 'Page',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    }),
+    defineField({
+      name: 'description',
+      title: 'Description',
+      type: 'text',
+    }),
+    defineField({
+      name: 'content',
+      title: 'Content',
+      type: 'text',
+    }),
+  ],
+})
